0633908259cab07dc6bbea955e3c966536db53b9
Natan Augusto <natanaugusto@gmail.com>
2022-09-18 19:08:49 -0300

    Added crontab

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
+# certbot --nginx -d yourdomain.com -d www.yourdomain.com
 
-if [ ! -f /etc/nginx/ssl/default.crt ]; then
-    openssl genrsa -out "/etc/nginx/ssl/default.key" 2048
-    openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
-    openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
-    chmod 644 /etc/nginx/ssl/default.key
-fi
-
-# Start crond in background
 crond -l 2 -b
-
-# Start nginx in foreground
 nginx
